da91872dc257e3f1dfd83c6b5cd4a016f5c70e53
minhnvgch17079 <minhnvgch17079@fpt.edu.vn>
2019-08-28 22:13:22 +0700

    fix bug facebook
    sad

--- controllers/customer/controller-login.js
+++ controllers/customer/controller-login.js
@@ -56,6 +56,6 @@ module.exports.loginProcess = [
 
 module.exports.facebook = (req, res) => {
     let token = jwt.sign({id: req.user.id}, 'customer')
-    res.cookie('token', token)
+    res.cookie('token', token, {domain: 'atn-final.herokuapp.com'})
     res.redirect('/')
 }
